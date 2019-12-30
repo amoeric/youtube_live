14edc36978864638a66e3633b0208741572b2b12
Eric Syu <xeriok02390@gmail.com>
2019-12-30 15:45:58 +0800

    設定staging 路徑

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,2 +1,2 @@
 server ENV["STAGING_IP"], user: "deploy", roles: %w{app db web}, my_property: :my_value    #指定主機ip, 使用者
-set :deploy_to, "/home/deploy/youtube_live"
+set :deploy_to, "/data/store/deploy/youtube_live"
