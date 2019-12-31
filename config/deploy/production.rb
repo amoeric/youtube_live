921468fc12a82b36bedb78c0f0b1c7f10b1b970d
Eric Syu <xeriok02390@gmail.com>
2019-12-31 17:19:22 +0800

    production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
-server ENV["STAGING_IP"], user: "deploy", roles: %w{app db web}, my_property: :my_value    #指定主機ip, 使用者
-set :deploy_to, "/data/store/deploy/youtube_live"
+server ENV["PRODUCTION_IP"], user: "deploy", roles: %w{app db web}, my_property: :my_value    #指定主機ip, 使用者
+set :deploy_to, "/home/deploy/youtube_live"
